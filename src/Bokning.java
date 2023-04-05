d8f5c707fdaa53b5b9cce69feeadf8b3a4e70777
Nova Casserfelt <casserfelt.nova@elev.ga.ntig.se>
2023-04-05 14:49:06 +0200

    hej

--- src/Bokning.java
+++ src/Bokning.java
@@ -2,12 +2,17 @@ import java.util.Scanner;
 
 public class Bokning {
 
-    int pris = 0;
+    int pris;
+    int tid;
+    String kvitto;
 
     Scanner myScanner = new Scanner(System.in);
 
     public Bokning(){
         klippning();
+        färgning();
+        datum();
+        //kvitto(); //länka till denhär i slutet av datum istället
     }
 
 
@@ -23,12 +28,50 @@ public class Bokning {
 
             if (frisyr == 1){
                 pris += 400;
-            }
-
-            if (frisyr == 2){
+                tid += 30;
+                kvitto += "Klippning: ny frisyr (+600kr)";
+            } else if (frisyr == 2){
                 pris += 200;
+                tid += 20;
+                kvitto += "Klippning: topparna (+400kr)";
             }
-        } else ...
+        } else if (klippning == 2) {
+            färgning();
+        }
+    }
+
+    public void färgning(){
+        System.out.println("Vill du färga håret? (1: Ja, 2: Nej)");
+        int färgning = myScanner.nextInt();
+
+        if(färgning == 1){
+            System.out.println("Vill du färga en naturlig färg eller inte? (1: Naturlig, 2: Onaturlig)");
+            int färg = myScanner.nextInt();
+
+            if(färg == 1){
+                pris += 1000;
+                tid += 120;
+                kvitto += "Färgning: naturlig färg (+1000kr)";
+            } else if (färg == 2){
+                pris += 2000;
+                tid += 120;
+                kvitto += "Färgning: onaturlig färg (+2000kr)";
+            }
+        } else if (färgning == 2){
+            datum();
+        }
+    }
+
+    public void datum(){
+
+    }
+
+    public void kvitto(){
+        System.out.println("Tack för din bokning! Här kommer ditt kvitto:");
+
+        if(){
+
+        }
     }
 
 }
--- src/Frisörer.java
+++ src/Frisörer.java
@@ -2,13 +2,15 @@ public class Frisörer {
 
     //Ska göra ett system där man blir tilldelar en frisör beroende av vilken tid man väljer
 
-    //Jobbar måndagar-onsdagar, mån: 11-18, tis: 09-15, ons: 14-20
+    //tis-sön 09-19
+
+    //Jobbar tis: 09-14,ons: 14-19, fre: 14-19, sön: 09-14
     String frisör1;
 
-    //Jobbar måndagar + onsdagar-fredagar, mån: 13-20, ons: 08-15, tor: 09-15, fre: 15-21
+    //Jobbar ons: 09-14, tor: 14-19, fre: 09-14, lör: 14-19
     String frisör2;
 
-    //Jobbar lör-sön, båda dagarna 11-18
+    //Jobbar tis: 14-19, tor: 09-14, lör: 09-14, sön: 14-19
     String frisör3;
 
     public Frisörer(){
