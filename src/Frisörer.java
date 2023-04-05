70cb96d45c096fc5f811b762d33ec6895b684d1f
Nova Casserfelt <casserfelt.nova@elev.ga.ntig.se>
2023-04-05 13:45:08 +0200

    hej

---
+++ src/Frisörer.java
@@ -0,0 +1,20 @@
+public class Frisörer {
+
+    //Ska göra ett system där man blir tilldelar en frisör beroende av vilken tid man väljer
+
+    //Jobbar måndagar-onsdagar, mån: 11-18, tis: 09-15, ons: 14-20
+    String frisör1;
+
+    //Jobbar måndagar + onsdagar-fredagar, mån: 13-20, ons: 08-15, tor: 09-15, fre: 15-21
+    String frisör2;
+
+    //Jobbar lör-sön, båda dagarna 11-18
+    String frisör3;
+
+    public Frisörer(){
+
+    }
+
+
+
+}
